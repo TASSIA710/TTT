d1d19ec23dc96bf72de3195aaf37ec3c0fc15189
Tassilo <tassia710@gmail.com>
2020-08-17 16:13:44 +0200

    Added oxygen plugin

---
+++ gamemode/core/plugins/oxygen/cl_init.lua
@@ -0,0 +1,39 @@
+
+--[[
+	TTT -> Oxygen Plugin (ClientSide)
+	by Tassilo (https://github.com/TASSIA710)
+--]]
+
+local OXYGEN_LOSE_PER_SECOND = 10
+local OXYGEN_GAIN_PER_SECOND = 5
+
+local drowning = false
+local start = 0
+local oxygen = 100
+
+net.Receive("TTT:Plugin:Oxygen:ToggleDrowning", function()
+	if net.ReadBool() then
+		Log.Debug("Started drowning.")
+		oxygen = math.min(oxygen + (CurTime() - start) * OXYGEN_GAIN_PER_SECOND, 100)
+		drowning = true
+		start = CurTime()
+	else
+		Log.Debug("Stopped drowning.")
+		oxygen = math.max(oxygen - (CurTime() - start) * OXYGEN_LOSE_PER_SECOND, 0)
+		drowning = false
+		start = CurTime()
+	end
+end)
+
+hook.Add("HUDPaint", "TTT:Plugin:Oxygen", function(ent, old, new)
+	if drowning then
+		local ox = math.max(oxygen - (CurTime() - start) * OXYGEN_LOSE_PER_SECOND, 0)
+		TTT.HUD.DrawRectangleBar(64, 64, 256, 32, "Oxygen: " .. math.Round(ox) .. "%", ox / 100, Color(0, 127, 195))
+		return
+	end
+
+	if oxygen == 100 then return end
+	local ox = math.min(oxygen + (CurTime() - start) * OXYGEN_GAIN_PER_SECOND, 100)
+	TTT.HUD.DrawRectangleBar(64, 64, 256, 32, "Oxygen: " .. math.Round(ox) .. "%", ox / 100, Color(0, 127, 195))
+	if ox == 100 then oxygen = 100 end
+end)
