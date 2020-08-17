9b59094416c2b57d24931924d732048af434f8fa
Tassilo <tassia710@gmail.com>
2020-08-17 14:49:41 +0200

    Included roles

--- gamemode/core/libraries/roles/shared.lua
+++ gamemode/core/libraries/roles/shared.lua
@@ -7,7 +7,19 @@
 TTT.Roles = {}
 
 
-
 -- Download files
+if SERVER then
+	AddCSLuaFile("roles/sh_fallback.lua")
+	AddCSLuaFile("roles/sh_innocent.lua")
+	AddCSLuaFile("roles/sh_traitor.lua")
+	AddCSLuaFile("roles/sh_detective.lua")
+	AddCSLuaFile("roles/sh_spectator.lua")
+end
+
 
 -- Include files
+include("roles/sh_fallback.lua")
+include("roles/sh_innocent.lua")
+include("roles/sh_traitor.lua")
+include("roles/sh_detective.lua")
+include("roles/sh_spectator.lua")
