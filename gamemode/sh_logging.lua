98a39d99883cbe4845b4fc8c89b2f3b732c55225
Tassilo <38081490+TASSIA710@users.noreply.github.com>
2020-08-25 00:12:53 +0200

    Update sh_logging.lua

--- gamemode/sh_logging.lua
+++ gamemode/sh_logging.lua
@@ -34,7 +34,7 @@ end
 
 
 --- Logs a message with the `INFO` level.
--- @param msg [varargs] the message to log
+-- @param msg [varargs] - the message to log
 function Log.Info(...)
 	if not log_info then return end
 	if SERVER and pterodactyl then print("\27[38;2;255;255;255m" .. varargsToString("  INFO", ...) .. "\27[0m")
@@ -44,7 +44,7 @@ end
 
 
 --- Logs a message with the `WARN` level.
--- @param msg [varargs] the message to log
+-- @param msg [varargs] - the message to log
 function Log.Warning(...)
 	if not log_warning then return end
 	if SERVER and pterodactyl then print("\27[38;2;255;191;0m" .. varargsToString("  WARN", ...) .. "\27[0m")
@@ -54,7 +54,7 @@ end
 
 
 --- Logs a message with the `ERROR` level.
--- @param msg [varargs] the message to log
+-- @param msg [varargs] - the message to log
 function Log.Error(...)
 	if not log_error then return end
 	if SERVER and pterodactyl then print("\27[38;2;255;0;0m" .. varargsToString(" ERROR", ...) .. "\27[0m")
@@ -64,7 +64,7 @@ end
 
 
 --- Logs a message with the `SEVERE` level.
--- @param msg [varargs] the message to log
+-- @param msg [varargs] - the message to log
 function Log.Severe(...)
 	if SERVER and pterodactyl then print("\27[101;93m" .. varargsToString("SEVERE", ...) .. "\27[0m")
 	else MsgC(Color(255, 0, 0), varargsToString("SEVERE", ...) .. "\n") end
@@ -73,7 +73,7 @@ end
 
 
 --- Logs a message with the `DEBUG` level.
--- @param msg [varargs] the message to log
+-- @param msg [varargs] - the message to log
 function Log.Debug(...)
 	if not log_debug then return end
 	if SERVER and pterodactyl then print("\27[38;2;127;127;127m" .. varargsToString(" DEBUG", ...) .. "\27[0m")
@@ -83,7 +83,7 @@ end
 
 
 --- Logs a message with the `NET` level.
--- @param msg [varargs] the message to log
+-- @param msg [varargs] - the message to log
 function Log.Net(...)
 	if not log_net then return end
 	if SERVER and pterodactyl then print("\27[38;2;127;127;255m" .. varargsToString("   NET", ...) .. "\27[0m")
