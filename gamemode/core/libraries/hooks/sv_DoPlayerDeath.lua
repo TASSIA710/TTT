0df9414c9e09414c225a79318a659083c4e7ec68
Tassilo <tassia710@gmail.com>
2020-08-25 17:37:12 +0200

    Fixed death cam

--- gamemode/core/libraries/hooks/sv_DoPlayerDeath.lua
+++ gamemode/core/libraries/hooks/sv_DoPlayerDeath.lua
@@ -6,7 +6,22 @@
 
 function GM:DoPlayerDeath(ply, attacker, dmg)
 
-	-- Create the players corpse
-	TTT.Corpses.CreateCorpse(ply, attacker, dmg)
+	-- TODO: Drop players weapons
+
+	-- Create the players corpse and make the player spectate that one
+	local corpse = TTT.Corpses.CreateCorpse(ply, attacker, dmg)
+	if IsValid(corpse) then
+		ply:Spectate(OBS_MODE_CHASE)
+		ply:SpectateEntity(corpse)
+	else
+		ply:Spectate(OBS_MODE_ROAMING)
+	end
+
+	-- Strip current weapons from ply
+	ply:StripWeapons()
+
+	-- TODO: Award killer with credits
+
+	-- TODO: Send last words
 
 end
